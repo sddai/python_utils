29508d309be1e9ed54aaf530e16df3d3ade2eece
sddai <sida.dai.86@gmail.com>
2019-02-02 14:48:26 +0800

    Create batch_rename_files.py

---
+++ batch_rename_files.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+class ImageRename():
+    def __init__(self):
+        self.path = 'D:/xpu/paper/plate_data'
+
+    def rename(self):
+        filelist = os.listdir(self.path)
+        total_num = len(filelist)
+
+        i = 0
+        for item in filelist:
+            if item.endswith('.jpg'):
+                src = os.path.join(os.path.abspath(self.path), item)
+                dst = os.path.join(os.path.abspath(self.path), '0000' + format(str(i), '0>3s') + '.jpg')
+                os.rename(src, dst)
+                print 'converting %s to %s ...' % (src, dst)
+                i = i + 1
+        print 'total %d to rename & converted %d jpgs' % (total_num, i)
+
+if __name__ == '__main__':
+    newname = ImageRename()
+    newname.rename()
